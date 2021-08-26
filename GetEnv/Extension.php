ce9e6a2a3f553cd46b8d03bd95785c4287f150b3
Davis Zalitis <davis.zalitis@lattelecom.lv>
2021-08-26 12:33:39 +0300

    move getenv

---
+++ GetEnv/Extension.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types = 1);
+
+namespace Vairogs\Twig\GetEnv;
+
+use JetBrains\PhpStorm\Pure;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+use Vairogs\Component\Utils\Twig\TwigTrait;
+use Vairogs\Component\Utils\Vairogs;
+use function getenv;
+
+class Extension extends AbstractExtension
+{
+    use TwigTrait;
+
+    public function getFunctions(): array
+    {
+        $input = [
+            'getenv' => 'getEnv',
+        ];
+
+        return $this->makeArray($input, Vairogs::RAVEN, TwigFunction::class);
+    }
+
+    #[Pure]
+    public function getEnv(string $varname): mixed
+    {
+        if ($env = getenv($varname)) {
+            return $env;
+        }
+
+        return $_ENV[$varname] ?? $varname;
+    }
+}
